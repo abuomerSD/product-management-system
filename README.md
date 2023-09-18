fe24e998e7a9aa43da92ef898131578f6a1b1d41
asdf <tayeb1293@yahoo.com>
2023-09-18 06:03:34 +0200

    working on the project structer

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 product management system using node js at backend and javafx at frontend
+
+product:
+
+- id
+- name
+- price
+- image
--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+// using morgan logging
+app.use(morgan('dev'));
 
 
+app.get('/products', (req, res) => {
+    res.json({
+        msg:'/products'
+    })
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/products');
+});
+
+app.get('/products/:id', (req, res) => {
+    res.json({
+        msg: '/products/:id'
+    })
+});
+
+app.post('/products', (req, res) => {
+    res.json({
+        msg: 'post /products'
+    });
+    res.status(404);
+});
+
+app.put('/products/:id', (req, res) => {
+    res.json({
+        msg:'put /products/:id'
+    })
+});
+
+app.delete('/products/:id', (req, res) => {
+    res.json({
+        msg:'delete /products/:id'
+    })
+});
+
+app.use((req,res) => {
+    res.json({
+        msg:"404"
+    })
+});
 
 const port = 5000;
-app.listen(port, ()=> console.log(`Server is Listening to Requests at Port : ${port}`))
+app.listen(port, ()=> console.log(`Server is Listening to Requests at Port : ${port}`));
